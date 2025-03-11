dc8ff2ea06b31c5ea6dd16b5e264b528764a7a13
swegggggg12 <42904@prot.ac.th>
2025-03-11 15:05:58 +0700

    Add files via upload

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,7 @@
+number = int(intput("less than 25:"))
+if number > 25:
+    print("Eroror")
+else:
+     while number <= 25:
+          print(f"inside the loop, my variable is {number}")
+          number += 1
