134e93213a60eb545d86841e9dd744ecb8b74ec8
swegggggg12 <42904@prot.ac.th>
2025-03-12 09:44:32 +0700

    Add files via upload

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,7 @@
+num1 = float (input("Give me the first number: "))
+num2 = float (input("Give me the second number: "))
+
+print(f"{num1}  + {num2} = {num1+num2}")
+print(f"{num1}  - {num2} = {num1-num2}")
+print(f"{num1}  * {num2} = {num1*num2}")
+print(f"{num1}  / {num2} = {num1/num2}")
